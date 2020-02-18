d79d11f9436bf67ff18720759908ad293439986b
Arturo Serna Leon <Arturo@funda.nl>
2020-02-18 17:20:20 +0100

    Minify all the things

--- src/build.js
+++ src/build.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Handlebars = require("handlebars");
 const UglifyJS = require("uglify-js");
+var minify = require('html-minifier').minify;
 
 var contents = fs.readFileSync('./index.handlebars', 'utf8');
 const template = Handlebars.compile(contents);
@@ -12,7 +13,10 @@ function generateHtml(inputFile,outputFile){
     Handlebars.registerPartial(input.introSrc, intro);
     
     const result = template(input);
-    fs.writeFileSync(outputFile, result, { mode: 0o755 }); 
+    var minHtml = minify(result, {
+        removeAttributeQuotes: true
+      });
+    fs.writeFileSync(outputFile, minHtml, { mode: 0o755 }); 
 }
 
 generateHtml('./es.json','../index.html');
@@ -22,3 +26,10 @@ generateHtml('./en.json','../en.html');
 var js = fs.readFileSync('./files/script.js', 'utf8');
 var minJs = UglifyJS.minify(js);
 fs.writeFileSync('../files/script.js', minJs.code, { mode: 0o755 }); 
+
+var uglifycss = require('uglifycss');
+var uglified = uglifycss.processFiles(
+    [ './files/reset.css', 
+    './files/style.css' ]
+);
+fs.writeFileSync('../files/min.css', uglified, { mode: 0o755 }); 
