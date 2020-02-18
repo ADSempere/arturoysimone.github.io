3f8fc0b3d739a44fbffcb1b11e1d57b523a27a2c
Arturo Serna Leon <Arturo@funda.nl>
2020-02-18 15:20:00 +0100

    Update template and es input file

--- src/build.js
+++ src/build.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const Handlebars = require("handlebars");
-const es = require('./es.json');
 
 var contents = fs.readFileSync('./index.handlebars', 'utf8');
 const template = Handlebars.compile(contents);
 
-var introEs = fs.readFileSync('./intro.es.handlebars', 'utf8');
-Handlebars.registerPartial("intro.es", introEs);
+function generateHtml(inputFile,outputFile){
+    const input = require(inputFile);
 
-const esResult = template(es);
-fs.writeFileSync('../index.html', esResult, { mode: 0o755 });
+    const intro = fs.readFileSync('./'+input.introSrc, 'utf8');
+    Handlebars.registerPartial(input.introSrc, intro);
+    
+    const result = template(input);
+    fs.writeFileSync(outputFile, result, { mode: 0o755 }); 
+}
 
-console.log(esResult);
+generateHtml('./es.json','../index.html');
